d8645f9a8610bd0c0bf0f2e6907afc736a0cda5d
Kennedy Tedesco <kennedyt.tw@gmail.com>
2016-12-21 22:41:47 -0200

    Clean up interface

--- src/Interfaces/PresenterInterface.php
+++ src/Interfaces/PresenterInterface.php
@@ -4,16 +4,4 @@ namespace KennedyTedesco\Presenter\Interfaces;
 
 interface PresenterInterface
 {
-    /**
-     * @param $property
-     * @return mixed
-     */
-    public function __get($property);
-
-    /**
-     * @param $method
-     * @param $arguments
-     * @return mixed
-     */
-    public function __call($method, $arguments);
 }
